9021e40918fea3539f4ed9383804b58f8d7a4c83
21zhichua <91635596+21zhichua@users.noreply.github.com>
2022-12-13 20:42:18 -0800

    Update GPACalculator.java

--- GPACalculator.java
+++ GPACalculator.java
@@ -1,61 +1,49 @@
 import java.util.Scanner;
 public class GPACalculator {
-    private double numOfPeriods;
+    private int numOfPeriods;
     private String grades;
 
-    double totalGrade;
-
-    double classGrade;
-    public GPACalculator(double numOfPeriods, String grades) {
+    private double totalGrade;
+    private double classGrade;
+    public GPACalculator(int numOfPeriods, String grades) {
         this.numOfPeriods = numOfPeriods;
         this.grades = grades;
     }
-    public double breakdownString(double totalGrade, String grades, double classGrade) {
-        for (int i = 0; i < numOfPeriods; i++) {
+    public double breakdownString(String grades) {
+        double totalGrade = 0;
+        for (int i = 0; i < grades.length(); i++) {
             if (grades.charAt(i) == 'A') {
-                classGrade = 4.0;
-                totalGrade += classGrade;
+                totalGrade += 4.0;
             }
             else if (grades.charAt(i) == 'B') {
-                classGrade = 3.0;
-                totalGrade += classGrade;
+                totalGrade += 3.0;
             }
             else if (grades.charAt(i) == 'C') {
-                classGrade = 2.0;
-                totalGrade += classGrade;
+                totalGrade += 2.0;
             }
             else if (grades.charAt(i) == 'D') {
-                classGrade = 1.0;
-                totalGrade += classGrade;
+                totalGrade += 1.0;
             }
-            else if (grades.charAt(i) == 'F') {
-                classGrade = 0.0;
-                totalGrade += classGrade;
-            }
-
         }
         return totalGrade;
     }
-    public double calculateGPA(double totalGrade) {
-        double GPA = 4.0 / 4.0;
+    public double calculateGPA(double totalGrade,int numOfPeriods) {
+        double GPA = totalGrade / numOfPeriods;
         return GPA;
     }
 
 
     public static void main(String args[]) {
-        double newPeriods;
+        int newPeriods;
         String newGrades;
-        double newTotal = 0;
-        double newResult = 0;
-        double newClass = 0;
+        double newTotal;
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your number of periods: ");
-        newPeriods = input.nextDouble();
+        newPeriods = input.nextInt();
         System.out.print("Enter your grades (A,B,C,F,A,B): ");
         newGrades = input.next();
-        GPACalculator newGPA = new GPACalculator(newPeriods, newGrades);
-        newGPA.breakdownString(newTotal, newGrades, newClass);
-        newGPA.calculateGPA(newTotal);
-        System.out.println(newResult);
+        GPACalculator newCalc = new GPACalculator(newPeriods,newGrades);
+        newTotal = newCalc.breakdownString(newGrades);
+        System.out.println(newCalc.calculateGPA(newTotal,newPeriods));
     }
 }
